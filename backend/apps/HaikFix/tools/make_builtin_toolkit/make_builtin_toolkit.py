8fa1c26d2efd3dbf443c444d8a6af676f16f236a
haikdc <haikdecie@gmail.com>
2026-04-02 16:32:54 -0700

    [Haik]: cpkt, organized the builtin_toolkits, now onto session management

--- backend/apps/HaikFix/tools/make_builtin_toolkit/make_builtin_toolkit.py
+++ backend/apps/HaikFix/tools/make_builtin_toolkit/make_builtin_toolkit.py
@@ -6,12 +6,12 @@ from backend.apps.HaikFix.tools.shared_structs.Toolkit import Toolkit
 from backend.apps.HaikFix.tools.make_builtin_toolkit.open_swarm_toolkits.agents_toolkit.make_agents_toolkit import make_agents_toolkit
 from backend.apps.HaikFix.tools.make_builtin_toolkit.open_swarm_toolkits.browser_toolkit.make_browser_toolkit import make_browser_toolkit
 
-from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.FILESYSTEM_TOOLKIT import FILESYSTEM_TOOLKIT
-from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.INTERACTION_TOOLKIT import INTERACTION_TOOLKIT
-from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.PLANNING_TOOLKIT import PLANNING_TOOLKIT
-from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.SCHEDULING_TOOLKIT import SCHEDULING_TOOLKIT
-from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.SEARCH_TOOLKIT import SEARCH_TOOLKIT
-from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.SYSTEM_TOOLKIT import SYSTEM_TOOLKIT
+from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.basic_toolkits.FILESYSTEM_TOOLKIT import FILESYSTEM_TOOLKIT
+from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.meta_toolkits.INTERACTION_TOOLKIT import INTERACTION_TOOLKIT
+from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.meta_toolkits.PLANNING_TOOLKIT import PLANNING_TOOLKIT
+from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.meta_toolkits.SCHEDULING_TOOLKIT import SCHEDULING_TOOLKIT
+from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.basic_toolkits.SEARCH_TOOLKIT import SEARCH_TOOLKIT
+from backend.apps.HaikFix.tools.make_builtin_toolkit.pre_existing_toolkits.basic_toolkits.SYSTEM_TOOLKIT import SYSTEM_TOOLKIT
 from backend.apps.HaikFix.Agent.Agent import Agent
 
 @typechecked
