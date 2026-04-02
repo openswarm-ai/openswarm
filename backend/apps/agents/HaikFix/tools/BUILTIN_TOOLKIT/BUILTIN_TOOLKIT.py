175806bf89e23b739cc6565c815f400c89ce907f
haikdc <haikdecie@gmail.com>
2026-04-02 13:48:25 -0700

    [Haik]: ckpt, agent toolkit done. Onto browser

--- backend/apps/agents/HaikFix/tools/BUILTIN_TOOLKIT/BUILTIN_TOOLKIT.py
+++ backend/apps/agents/HaikFix/tools/BUILTIN_TOOLKIT/BUILTIN_TOOLKIT.py
@@ -1,24 +1,28 @@
-from backend.apps.agents.HaikFix.tools.shared_structs.Toolkit import Toolkit
-from backend.apps.agents.HaikFix.tools.shared_structs.Tool import Tool
+# BUILTIN_TOOLKIT.py
 
-from backend.apps.agents.HaikFix.tools.BUILTIN_TOOLKIT.components.AGENTS_TOOLKIT import AGENTS_TOOLKIT
+from typing import Dict
+from backend.apps.agents.HaikFix.tools.shared_structs.Toolkit import Toolkit
+from backend.apps.agents.HaikFix.tools.BUILTIN_TOOLKIT.components.AGENTS_TOOLKIT import make_agents_toolkit
 from backend.apps.agents.HaikFix.tools.BUILTIN_TOOLKIT.components.FILESYSTEM_TOOLKIT import FILESYSTEM_TOOLKIT
 from backend.apps.agents.HaikFix.tools.BUILTIN_TOOLKIT.components.INTERACTION_TOOLKIT import INTERACTION_TOOLKIT
 from backend.apps.agents.HaikFix.tools.BUILTIN_TOOLKIT.components.PLANNING_TOOLKIT import PLANNING_TOOLKIT
 from backend.apps.agents.HaikFix.tools.BUILTIN_TOOLKIT.components.SCHEDULING_TOOLKIT import SCHEDULING_TOOLKIT
 from backend.apps.agents.HaikFix.tools.BUILTIN_TOOLKIT.components.SEARCH_TOOLKIT import SEARCH_TOOLKIT
 from backend.apps.agents.HaikFix.tools.BUILTIN_TOOLKIT.components.SYSTEM_TOOLKIT import SYSTEM_TOOLKIT
+from backend.apps.agents.HaikFix.Agent.Agent import Agent
 
-BUILTIN_TOOLKIT = Toolkit(
-    name="builtin",
-    description="Builtin tools",
-    nested_toolkits=[
-        AGENTS_TOOLKIT,
-        FILESYSTEM_TOOLKIT,
-        INTERACTION_TOOLKIT,
-        PLANNING_TOOLKIT,
-        SCHEDULING_TOOLKIT,
-        SEARCH_TOOLKIT,
-        SYSTEM_TOOLKIT,
-    ]
-)
+
+def make_builtin_toolkit(parent: Agent, agent_registry: Dict[str, Agent]) -> Toolkit:
+    return Toolkit(
+        name="builtin",
+        description="Builtin tools",
+        nested_toolkits=[
+            make_agents_toolkit(parent, agent_registry),
+            FILESYSTEM_TOOLKIT,
+            INTERACTION_TOOLKIT,
+            PLANNING_TOOLKIT,
+            SCHEDULING_TOOLKIT,
+            SEARCH_TOOLKIT,
+            SYSTEM_TOOLKIT,
+        ],
+    )
